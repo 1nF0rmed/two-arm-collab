a341dba07ce15236967c45231423ed335d30306b
1nF0rmed <prkinformed@gmail.com>
2025-06-21 12:47:29 +0530

    update: add camera config

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 This repo is to demonstrate bimanual collaboration using the SO-101 Lerobot arms.
 
 It also includes a single example to pick_up objects on a single arm.
+
+## Setup
+
+### Install LeRobot
+
+Clone LeRobot in a directory above
+```bash
+git clone https://github.com/huggingface/lerobot.git
+```
+
+Move into this repo and install LeRobot + deps like so
+```bash
+pip install -e ../lerobot/".[feetech,smolvla,aloha,pi0,hilserl]"
+```
+
+### Find your cameras
+
+```bash
+python -m lerobot.find_cameras opencv
+```
--- single_pickup_box/pick_up_teleop.py
+++ single_pickup_box/pick_up_teleop.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+from lerobot.common.cameras.opencv.configuration_opencv import OpenCVCameraConfig
 from lerobot.common.teleoperators.so101_leader import SO101LeaderConfig, SO101Leader
 from lerobot.common.robots.so101_follower import SO101FollowerConfig, SO101Follower
 
@@ -12,9 +13,18 @@ ROBOT_PORT = os.getenv("ROBOT_PORT")
 TELEOP_ID = os.getenv("TELEOP_ID")
 TELEOP_PORT = os.getenv("TELEOP_PORT")
 
+WRIST_CAMERA_INDEX = int(os.getenv("WRIST_CAMERA_INDEX"))
+TOP_CAMERA_INDEX = int(os.getenv("TOP_CAMERA_INDEX"))
+
+camera_config = {
+    "wrist": OpenCVCameraConfig(index_or_path=WRIST_CAMERA_INDEX, width=640, height=480, fps=30),
+    "top": OpenCVCameraConfig(index_or_path=TOP_CAMERA_INDEX, width=640, height=480, fps=30),
+}
+
 robot_config = SO101FollowerConfig(
     id=ROBOT_ID,
     port=ROBOT_PORT,
+    cameras=camera_config,
 )
 
 teleop_config = SO101LeaderConfig(
@@ -29,5 +39,6 @@ robot.connect()
 teleop.connect()
 
 while True:
+    observation = robot.get_observation()
     action = teleop.get_action()
     robot.send_action(action)
